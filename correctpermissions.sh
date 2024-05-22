612ab1cd8cbac5481c2f82ca266db1be9b2f937a
alumno <alumno@debian>
2024-05-22 14:12:21 +0200

    prueba

--- correctpermissions.sh
+++ correctpermissions.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 // Adding current user to www-data
-sudo adduser $USER www-data
+adduser $USER www-data
 
 //change ownership to user:www-data and 
-sudo chown $USER:www-data -R /var/www/html
-sudo chmod u=rwX,g=srX,o=rX -R /var/www/html
+chown $USER:www-data -R /var/www/html
+chmod u=rwX,g=srX,o=rX -R /var/www/html
 
 // change file permissions of existing files and folders to 755/644
-sudo find /var/www/html -type d -exec chmod g=rwxs "{}" \;
-sudo find /var/www/html -type f -exec chmod g=rws "{}" \;
+find /var/www/html -type d -exec chmod g=rwxs "{}" \;
+find /var/www/html -type f -exec chmod g=rws "{}" \;
