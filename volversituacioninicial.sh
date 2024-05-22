c3155233960b9507aa6a66a763329d3713b9ce47
alumno <alumno@debian>
2024-05-22 16:47:46 +0200

    prueba

---
+++ volversituacioninicial.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#// Adding current user to www-data
+id_user=root
+id_group=root
+
+#//change ownership to user:www-data and 
+chown $id_user:$id_group -R /var/www/html
+chmod u=rwX,g=srX,o=rX -R /var/www/html
+
+#// change file permissions of existing files and folders to 755/644
+find /var/www/html -type d -exec chmod g=rwxs "{}" \;
+find /var/www/html -type f -exec chmod g=rws "{}" \;
