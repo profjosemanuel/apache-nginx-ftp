0837dc6029d5523edb422533f8d0d15abe0ede31
profjosemanuel <97788933+profjosemanuel@users.noreply.github.com>
2024-05-26 14:57:50 +0200

    Create correctpermissions_alumno.sh

---
+++ correctpermissions_alumno.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+// Adding current user to www-data
+#adduser $USER www-data
+
+//change ownership to user:www-data and 
+chown $root:$alumno -R /var/www/html
+chmod u=rwX,g=srX,o=rX -R /var/www/html
+
+// change file permissions of existing files and folders to 755/644
+find /var/www/html -type d -exec chmod g=rwxs "{}" \;
+find /var/www/html -type f -exec chmod g=rws "{}" \;
